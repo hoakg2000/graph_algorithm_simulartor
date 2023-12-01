e778303da3a74f2d34457332d1db99cdb25cf862
hoakg2000 <hoakg2000@gmail.com>
2023-12-01 09:19:40 +0700

    init

---
+++ src/main/java/jacky/algo/sim/SimApplication.java
@@ -0,0 +1,13 @@
+package jacky.algo.sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimApplication.class, args);
+	}
+
+}
